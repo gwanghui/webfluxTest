16f89c2eb540544b149312c2b864a4d5e77180be
kwang <park776139@gmail.com>
2018-11-21 20:31:04 +0900

    Merge branch 'master' of https://github.com/gwanghui/webfluxTest

--- src/main/java/kwang/example/webfluxdemo/WebfluxdemoApplication.java
+++ src/main/java/kwang/example/webfluxdemo/WebfluxdemoApplication.java
@@ -2,8 +2,10 @@ package kwang.example.webfluxdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class WebfluxdemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/kwang/example/webfluxdemo/controller/MessageController.java
@@ -0,0 +1,7 @@
+package kwang.example.webfluxdemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+}
